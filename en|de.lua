fa21a0284701035a441e65dd775b1aed6ced22f9
Gumicuci <gumicuci2@gmail.com>
2025-03-18 17:28:30 +0000

    Update en|de.lua

--- en|de.lua
+++ en|de.lua
@@ -1,7 +1,10 @@
 local array = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ";", ":", "'", ",", ".", "<", ">", "/", "?", "|", "`", "~", "£", "€", " "}
 local array2 = {"$", "1", "y", "x", "6", "a", "l", "4", "U", "u", "J", "p", "&", "=", "f", "m", "I", "|", "X", "5", "(", "F", "_", "s", "/", "9", "<", "P", ")", "v", "E", ";", "-", "W", "S", "r", "0", "c", "e", "C", "3", "Z", "/", "[", "G", "8", "T", "b", "^", "j", "i", "{", "L", "o", "V", "€", ".", "Y", "z", "+", "h", "t", "q", "R", "d", "}", "D", ",", "%", "#", "k", "B", "n", "Q", "£", "*", "M", "H", "g", "O", "w", "`", "@", "2", "A", ">", "'", "!", "K", "7", ":", "N", "?", "~", "]"}
-
+local kickmsg = {"Fuck off.", "Fuck off", "Piss off admin.", "Invalid script skid!", "Skid alert!!"}
 getgenv().encode = function(input : string, gameversion : number)
+  if gameversion == nil or not game.PlaceVersion / 2 then
+	  game.Players.LocalPlayer:Kick(kickmsg[math.random(1, 5)])
+  end
   local result = ""
   for i = 1, #input do
     local char = input:sub(i, i)
@@ -18,6 +21,9 @@ getgenv().encode = function(input : string, gameversion : number)
 end
 
 getgenv().decode = function(input : string, gameversion : number)
+  if gameversion == nil or not game.PlaceVersion / 2 then
+	  game.Players.LocalPlayer:Kick(kickmsg[math.random(0, 5)])
+  end
   local result = ""
   for i = 1, #input do
     local char = input:sub(i, i)
