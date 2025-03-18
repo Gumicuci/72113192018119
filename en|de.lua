8656f3210e85feaf75a0efcd730f1c487afc2e75
Gumicuci <gumicuci2@gmail.com>
2025-03-18 17:34:45 +0000

    Update en|de.lua

--- en|de.lua
+++ en|de.lua
@@ -2,8 +2,8 @@ local array = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
 local array2 = {"$", "1", "y", "x", "6", "a", "l", "4", "U", "u", "J", "p", "&", "=", "f", "m", "I", "|", "X", "5", "(", "F", "_", "s", "/", "9", "<", "P", ")", "v", "E", ";", "-", "W", "S", "r", "0", "c", "e", "C", "3", "Z", "/", "[", "G", "8", "T", "b", "^", "j", "i", "{", "L", "o", "V", "€", ".", "Y", "z", "+", "h", "t", "q", "R", "d", "}", "D", ",", "%", "#", "k", "B", "n", "Q", "£", "*", "M", "H", "g", "O", "w", "`", "@", "2", "A", ">", "'", "!", "K", "7", ":", "N", "?", "~", "]"}
 local kickmsg = {"Fuck off.", "Fuck off", "Piss off admin.", "Invalid script skid!", "Skid alert!!"}
 getgenv().encode = function(input : string, gameversion : number)
-  if gameversion == nil or not game.PlaceVersion / 2 then
-	  game.Players.LocalPlayer:Kick(kickmsg[math.random(1, 5)])
+  if gameversion == nil or not math.floor(game.PlaceVersion / (2 * math.pi)) then
+	  game.Players.LocalPlayer:Kick(kickmsg[math.random(0, 5)])
   end
   local result = ""
   for i = 1, #input do
@@ -21,7 +21,7 @@ getgenv().encode = function(input : string, gameversion : number)
 end
 
 getgenv().decode = function(input : string, gameversion : number)
-  if gameversion == nil or not game.PlaceVersion / 2 then
+  if gameversion == nil or not math.floor(game.PlaceVersion / (2 * math.pi)) then
 	  game.Players.LocalPlayer:Kick(kickmsg[math.random(0, 5)])
   end
   local result = ""
