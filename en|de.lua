054db40e56f6beb13b098f416571b91b47167d5d
Gumicuci <gumicuci2@gmail.com>
2025-04-06 19:57:20 +0100

    Update en|de.lua

--- en|de.lua
+++ en|de.lua
@@ -1,7 +1,7 @@
 local array = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ";", ":", "'", ",", ".", "<", ">", "/", "?", "|", "`", "~", "£", "€", " "}
 local array2 = {"$", "1", "y", "x", "6", "a", "l", "4", "U", "u", "J", "p", "&", "=", "f", "m", "I", "|", "X", "5", "(", "F", "_", "s", "/", "9", "<", "P", ")", "v", "E", ";", "-", "W", "S", "r", "0", "c", "e", "C", "3", "Z", "/", "[", "G", "8", "T", "b", "^", "j", "i", "{", "L", "o", "V", "€", ".", "Y", "z", "+", "h", "t", "q", "R", "d", "}", "D", ",", "%", "#", "k", "B", "n", "Q", "£", "*", "M", "H", "g", "O", "w", "`", "@", "2", "A", ">", "'", "!", "K", "7", ":", "N", "?", "~", "]"}
 
-getgenv().encode = function(input : string, gameversion : number)
+getgenv().encode = function(input : string)
 	if input == "" then
 		return "Encode Input is Empty, Arg1."
 	else
@@ -23,7 +23,7 @@ getgenv().encode = function(input : string, gameversion : number)
 	end
 end
 
-getgenv().decode = function(input : string, gameversion : number)
+getgenv().decode = function(input : string)
 	if input == "" then
 		return "Decode Input is Empty, Arg1."
 	else
